ff58cc7edab258e002b58db9ab916a74eb392044
JC Webb <jcwebb@JCs-iMac.local>
2020-06-24 14:45:46 -0700

    kopolo media mention

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("JCWebb.com on its way");
+    res.send("JCWebb.com site by Kopolo Media-- coming soon!");
 });
 
 const port = process.env.port || 3000;
