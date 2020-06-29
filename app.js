124c7be715bd3cda4a78cbf563258278b0cea6b7
JC Webb <jcwebb@JCs-iMac.local>
2020-06-28 19:53:50 -0700

    for Christian men

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("JCWebb.com site by Kopolo Media-- coming soon!");
+    res.send("JCWebb.com is a media site for Christian men");
 });
 
 const port = process.env.port || 3000;
